4d52ebc3b333ce8e88e1bd47eb863e4bc3ef7218
thuannguyen18 <116961820+thuannguyen18@users.noreply.github.com>
2023-08-22 00:31:55 +0700

    Update UI: user settings page

--- frontend/src/pages/user/settings/index.jsx
+++ frontend/src/pages/user/settings/index.jsx
@@ -1,60 +1,77 @@
 import React, { useState } from "react";
 import Container from "components/Container";
 import { AiOutlineCamera } from "react-icons/ai";
-import Input from "components/Input";
+
+const options = [
+    {
+        id: "username",
+        htmlFor: "username",
+        value: "Nguyen Huynh Minh Thuan",
+        type: "text",
+    },
+    {
+        id: "email",
+        htmlFor: "email",
+        value: "nguyenmthuan18@gmail.com",
+        type: "email",
+    },
+];
+
+const passwords = [
+    {
+        id: "password",
+        htmlFor: "password",
+        placeholder: "**********",
+        type: "password",
+    },
+    {
+        id: "new password",
+        htmlFor: "new password",
+        placeholder: "**********",
+        type: "password",
+    },
+    {
+        id: "confirm new password",
+        htmlFor: "confirm new password",
+        placeholder: "**********",
+        type: "password",
+    },
+];
 
 export default function Settings() {
     const [isOpen, setIsOpen] = useState(false);
+    const [avatar, setAvatar] = useState();
+ 
+    const handleFile = (e) => {
+        const fileUpload = e.target.files[0];
+        console.log(fileUpload)
+        const previewUrl = URL.createObjectURL(fileUpload);
+    
+        fileUpload.preview = previewUrl;
+        setAvatar(fileUpload);
+    }
 
-    const options = [
-        {
-            id: "username",
-            htmlFor: "username",
-            value: "Nguyen Huynh Minh Thuan",
-            type: "text",
-        },
-        {
-            id: "email",
-            htmlFor: "email",
-            value: "nguyenmthuan18@gmail.com",
-            type: "email",
-        },
-    ];
-
-    const passwords = [
-        {
-            id: "password",
-            htmlFor: "password",
-            placeholder: "**********",
-            type: "password",
-        },
-        {
-            id: "new password",
-            htmlFor: "new password",
-            placeholder: "**********",
-            type: "password",
-        },
-        {
-            id: "confirm new password",
-            htmlFor: "confirm new password",
-            placeholder: "**********",
-            type: "password",
-        },
-    ]
+    const disable = 
+    <button type="button" class="dark:bg-blue-500 cursor-not-allowed" disabled>Disabled button</button>
+    
 
     return (
         <Container>
             <div className="mx-auto mt-10 md:w-[720px] overflow-hidden p-8">
-                <div className="w-32 h-32 bg-gray-400 rounded-full">
-                    <div className="w-full h-full flex items-center justify-center transition duration-300 ease-in-out hover:scale-110 cursor-pointer">
-                        <AiOutlineCamera className="w-12 h-12 text-gray-300" />
-                    </div>
+                <div className="flex items-center justify-center w-32 h-32">
+                    <label htmlFor="dropzone-file" className="flex flex-col items-center rounded-full justify-center w-full h-full border-2 border-gray-300 cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                        {!avatar && <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                            <AiOutlineCamera className="w-12 h-12 text-gray-300" />
+                        </div>}
+                        {avatar && <img className="w-full h-full object-cover rounded-full" src={avatar.preview} alt="avatar" />}
+                        <input id="dropzone-file" type="file" className="hidden" onChange={handleFile} />
+                    </label>
                 </div>
                 <div className="md:grid md:grid-cols-2 md:gap-6 mt-12">
                     {options.map(option => (
                         <div key={option.id} className="col-span-1">
                             <label className="block text-xs uppercase font-semibold mb-1 text-gray-600" htmlFor={option.htmlFor}>{option.id}</label>
-                            <input className="w-full h-12 py-2 px-3 bg-gray-350 border border-gray-300 rounded outline-none focus:bg-white mb-4" value={option.value} type={option.type} id={option.id} />
+                            <input className="w-full h-12 py-2 px-3 bg-gray-350 border border-gray-300 rounded outline-none focus:bg-white mb-4" defaultValue={option.value} type={option.type} id={option.id} />
                         </div>
                     ))}
                 </div>
@@ -71,14 +88,14 @@ export default function Settings() {
                             <input className="w-full h-12 py-2 px-3 bg-gray-350 border border-gray-300 rounded outline-none focus:bg-white mb-4" placeholder={password.placeholder} type={password.type} id={password.id} />
                         </div>
                     ))}
-                    <button className="w-full h-10 rounded-full bg-sky-500 text-white hover:bg-sky-600">Change</button>
+                    <button type="button" className="w-full text-white bg-sky-300 dark:bg-sky-500 cursor-not-allowed rounded-full px-5 py-2.5 text-center" disabled>Change</button>
                 </React.Fragment>}
                 <div className="flex flex-row-reverse py-6">
-                    <button className="bg-sky-500 text-white rounded-full h-10 px-6 ml-3">Update</button>
+                    <button type="button" className="text-white h-10 px-6 bg-sky-300 dark:bg-sky-500 cursor-not-allowed rounded-full ml-2" disabled>Update</button>
                     <button className="h-10 px-6 rounded-full shadow border hover:bg-gray-200">Cancel</button>
                 </div>
                 <div className="border-t border-gray-300 mt-5">
-                    <button className="h-12 w-full border border-gray-200 rounded mt-5 bg-red-500 text-white">
+                    <button className="h-12 w-full border border-gray-200 rounded mt-5 bg-red-300 text-white">
                         Delete account
                     </button>
                 </div>
